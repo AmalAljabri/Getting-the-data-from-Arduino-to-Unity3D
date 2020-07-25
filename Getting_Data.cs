e5f74e676ff04091c769d10206fac6cfdae178e8
AmalAljabri <61520524+AmalAljabri@users.noreply.github.com>
2020-07-25 22:52:23 +0300

    Update Getting_Data.cs

--- Getting_Data.cs
+++ Getting_Data.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class Getting_Data : MonoBehaviour
 {
