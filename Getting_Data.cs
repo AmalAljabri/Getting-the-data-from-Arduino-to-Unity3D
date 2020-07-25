75d1f630f6d02ba352c6392972cdd16bcb86e734
AmalAljabri <61520524+AmalAljabri@users.noreply.github.com>
2020-07-25 21:16:54 +0300

    Update Getting_Data.cs

--- Getting_Data.cs
+++ Getting_Data.cs
@@ -1,1 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class Getting_Data : MonoBehaviour { 
+    public string url = "http://172.20.10.9/ ";
+    public Text bpm;
+    public void open()
+{
+
+    StartCoroutine(GetRequest(url));
+
+}
+
+IEnumerator GetRequest(string uri)
+{
+
+    while (true)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+            bpm.text = webRequest.downloadHandler.text;
+            Debug.Log(webRequest.downloadHandler.text);
+            yield return new WaitForSeconds(1);
+           
+
+        }
+
+    }
+
+}
+}
